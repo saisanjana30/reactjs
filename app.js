4d090431bf53cbb142c8bd9723d8b2facda6ee01
saisanjana30 <57140717+saisanjana30@users.noreply.github.com>
2022-01-12 20:45:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var request = new XMLHttpRequest();
+/*var request = new XMLHttpRequest();
 request.open('GET','https://api.openweathermap.org/data/2.5/weather?q=Hyderabad&appid=93f26e3c57081a6210de53b8dcfdfea4',true);
 
 request.onload = function() {
@@ -22,4 +22,33 @@ request.onload = function() {
 };
 
 
+request.send();*/
+let request = new XMLHttpRequest();
+
+//open a connection
+request.open('GET','https://reqres.in/api/users?page=2',true);
+
+request.onload = function() {
+    if( request.status >=200 && request.status < 400) {
+        //get the data
+        console.log(" success!!!");
+        let data = JSON.parse( request.responseText);
+      /*  let imgsrc = 'https://openweathermap.org/img/w/'+data.weather[0].icon+'.png';
+        document.getElementById('mypara').innerHTML = data.main.temp+'F';
+        document.getElementById('myimg').src= imgsrc;
+*/
+        console.log(data);
+
+
+    }
+    else {
+        console.log(" could not connect to server")
+    }
+}
+// error
+request.onerror = function() {
+    console.log("error!!")
+}
+
+//send the request
 request.send();
